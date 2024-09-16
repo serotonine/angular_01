ebbf873b8487b570b6d7510525a5273c5b1ebc63
serotonine <ks.serotonine@gmail.com>
2024-09-16 13:03:07 +0200

    Master: Replace files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = '01';
-}
+export class AppComponent {}
